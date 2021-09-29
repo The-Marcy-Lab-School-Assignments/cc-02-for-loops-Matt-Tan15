cfd0c0505b0eb39e886475ba76f1ed713db2511e
Matthew Tan <tanmatt804@gmail.com>
2021-09-29 14:48:59 +0000

    finished

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 //1
 function countToTen(){
-  
+  for(let i = 1; i <= 10; i++){
+      console.log(i)
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+  for(let i = 1; i <= num; i++){
+      console.log(i)
+  }
 }
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(even){
+  for(let i = 1; i <= even; i++){
+      if(i % 2 === 0){
+          console.log(i)
+      }
+  }
 }
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(odd){
+  for(let i = 1; i<= odd; i++){
+      if(i %2 != 0){
+          console.log(i)
+      }
+  }
 }
 
 //5 
-function countEvens(){
-  
+function countEvens(arr){
+  let evenCount = 0
+  for(let i = 0; i<= arr.length; i++){
+      if(i %2 === 0){
+          evenCount++
+      }
+  } return evenCount
 }
-
